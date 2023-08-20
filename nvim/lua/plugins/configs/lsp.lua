1e27178041453ff2406acbc19aa9c341bd38ce8c
0xYami <0xYami@proton.me>
2023-08-20 02:24:21 +0200

    fix(nvim): add missing `cmp` capabilities

--- nvim/lua/plugins/configs/lsp.lua
+++ nvim/lua/plugins/configs/lsp.lua
@@ -30,13 +30,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason').setup()
 require('mason-lspconfig').setup({
   ensure_installed = { 'tsserver', 'astro', 'lua_ls', 'svelte' },
   handlers = {
     function(server)
-      lspconfig[server].setup({})
+      lspconfig[server].setup({
+        capabilities = capabilities,
+      })
     end,
     ['lua_ls'] = function()
       lspconfig.lua_ls.setup({
