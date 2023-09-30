7e8f6d17f4fdb9069cbc85127312b705d95ac47d
0xYami <yamiro.dev@gmail.com>
2023-09-30 21:44:59 +0200

    chore(fish): rename `mkdc` -> `mk`

--- fish/functions/mk.fish
+++ fish/functions/mk.fish
@@ -1,7 +1,7 @@
-function mkdc
+function mk
     if test (count $argv) -eq 1
         mkdir $argv[1]; and cd $argv[1]
     else
-        echo "Usage: mkdc <directory>"
+        echo "Usage: mk <directory>"
     end
 end
